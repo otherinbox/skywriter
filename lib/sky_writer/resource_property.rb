ed5b23e0049626941e713ea686e8ab945970bc22
Ryan Michael <kerinin@gmail.com>
2014-02-11 21:48:10 -0600

    Change property method to expect a :type key rather than :convert, since we can't know for sure how to convert the value into an appropriate JSON fragment

--- lib/sky_writer/resource_property.rb
+++ lib/sky_writer/resource_property.rb
@@ -11,7 +11,16 @@ module SkyWriter
     def as_json
       @as_json ||= property_definitions.each_with_object({}) do |property_definition, hash|
         if (value = @properties[property_definition.key])
-          hash[property_definition.name] = value.send(property_definition.convert)
+          case property_definition.type
+          when :string
+            hash[property_definition.name] = value.to_s
+          when :array
+            hash[property_definition.name] = Array(value)
+          when :integer
+            hash[property_definition.name] = value.to_i
+          when :float, :numeric
+            hash[property_definition.name] = value.to_f
+          end
         end
       end
     end
@@ -28,12 +37,12 @@ module SkyWriter
   end
 
   class PropertyDefinition
-    attr_reader :name, :key, :convert
+    attr_reader :name, :key, :type
 
     def initialize(name, **options)
       @name = name.to_s
       @key = name.to_s.underscore.to_sym
-      @convert = options[:convert] || :to_s
+      @type = options[:type] || :string
     end
   end
 end
