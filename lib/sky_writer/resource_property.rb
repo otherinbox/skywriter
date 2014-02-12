6e85b359b2c0ad7ac74fa2564414b459c953c22f
Ryan Michael <kerinin@gmail.com>
2014-02-11 22:11:09 -0600

    Provide #<property> accessor and #<property>_as_json accessor
    This will enable us to semantically define adhoc json conversions, such
    as:
    
    ``` ruby
    class Foo < Resource
      property "Bar"
      property "Baz"
    
      def bar_as_json
        if baz
          "custom value"
        else
          super
        end
      end
    end
    ```

--- lib/sky_writer/resource_property.rb
+++ lib/sky_writer/resource_property.rb
@@ -1,7 +1,19 @@
 module SkyWriter
   class ResourceProperty
     def self.property(name, **options)
-      property_definitions << PropertyDefinition.new(name, options)
+      property_definition = PropertyDefinition.new(name, options)
+
+      property_definitions << property_definition
+
+      # Define property accessor method (#foo)
+      define_method(property_definition.key) do 
+        property_value(property_definition)
+      end
+
+      # Define property json method (#foo_as_json)
+      define_method("#{property_definition.key}_as_json") do
+        property_as_json(property_definition)
+      end
     end
 
     def initialize(**options)
@@ -10,8 +22,8 @@ module SkyWriter
 
     def as_json
       @as_json ||= property_definitions.each_with_object({}) do |property_definition, hash|
-        if (value = @properties[property_definition.key])
-          hash[property_definition.name] = value.send(property_definition.convert)
+        if (value = property_as_json(property_definition))
+          hash[property_definition.name] = value
         end
       end
     end
@@ -20,6 +32,16 @@ module SkyWriter
 
     def self.property_definitions
       @property_definitions ||= []
+    end
+
+    def property_value(property_definition)
+      @properties[property_definition.key]
+    end
+
+    def property_as_json(property_definition)
+      if (value = property_value(property_definition)) && (!value.kind_of?(Enumerable) || !value.empty?)
+        value.send(property_definition.convert)
+      end
     end
 
     def property_definitions
