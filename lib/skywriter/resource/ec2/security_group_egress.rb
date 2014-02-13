2ca6d2cba1570a11cba59c76ecae2871814c788e
Ben Hamill <git-commits@benhamill.com>
2014-02-13 14:51:38 -0600

    Comments explaining alias thingies.

--- lib/skywriter/resource/ec2/security_group_egress.rb
+++ lib/skywriter/resource/ec2/security_group_egress.rb
@@ -8,6 +8,11 @@ module Skywriter
     end
   end
 
+  # This is just for aliasing purposes. Despite the fact that Amazon documents
+  # this class as a resource, it is, in fact, a resource property in all ways.
+  # So, we put it in the place you'd expect if you were looking at the
+  # CloudFormation documentations. And we're aliasing it below in the place
+  # where you'd expect it if you have your head right about how this behaves.
   class ResourceProperty
     module EC2
       SecurityGroupEgress = Skywriter::Resource::EC2::SecurityGroupEgress
--- lib/skywriter/resource/ec2/security_group_ingress.rb
+++ lib/skywriter/resource/ec2/security_group_ingress.rb
@@ -11,6 +11,11 @@ module Skywriter
     end
   end
 
+  # This is just for aliasing purposes. Despite the fact that Amazon documents
+  # this class as a resource, it is, in fact, a resource property in all ways.
+  # So, we put it in the place you'd expect if you were looking at the
+  # CloudFormation documentations. And we're aliasing it below in the place
+  # where you'd expect it if you have your head right about how this behaves.
   class ResourceProperty
     module EC2
       SecurityGroupIngress = Skywriter::Resource::EC2::SecurityGroupIngress
