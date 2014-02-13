1558efeae3a3511bb7c8556ba18868e1c7697cb8
Ben Hamill <git-commits@benhamill.com>
2014-02-13 14:32:15 -0600

    rm unused test file.

--- spec/skywriter/resource/elasti_cache/cache_cluster_spec.rb
+++
@@ -1,7 +0,0 @@
-require 'spec_helper'
-
-describe Skywriter::Resource::ElastiCache::CacheCluster do
-  it "is a Resource" do
-    expect(Skywriter::Resource::ElastiCache::CacheCluster.new('name')).to be_a(Skywriter::Resource)
-  end
-end
