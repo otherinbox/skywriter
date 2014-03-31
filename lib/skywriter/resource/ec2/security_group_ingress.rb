3c5e539b2f329796fdc623dfebb322047b448f6e
Peter Whiteside <peterwhiteside@otherinbox.com>
2014-03-31 16:37:51 -0500

    Security group rule resources and resource properties are not the same thing

--- lib/skywriter/resource/ec2/security_group_ingress.rb
+++ lib/skywriter/resource/ec2/security_group_ingress.rb
@@ -3,24 +3,19 @@ module Skywriter
     module EC2
       # AWS::EC2::SecurityGroupIngress Resource
       #
-      class SecurityGroupIngress < Skywriter::ResourceProperty::EC2::SecurityGroupRule
+      class SecurityGroupIngress
+        include Skywriter::Resource
+
         property :GroupName
         property :GroupId
+        property :IpProtocol
+        property :CidrIp
         property :SourceSecurityGroupName
         property :SourceSecurityGroupId
         property :SourceSecurityGroupOwnerId
+        property :FromPort
+        property :ToPort
       end
-    end
-  end
-
-  # This is just for aliasing purposes. Despite the fact that Amazon documents
-  # this class as a resource, it is, in fact, a resource property in all ways.
-  # So, we put it in the place you'd expect if you were looking at the
-  # CloudFormation documentations. And we're aliasing it below in the place
-  # where you'd expect it if you have your head right about how this behaves.
-  class ResourceProperty
-    module EC2
-      SecurityGroupIngress = Skywriter::Resource::EC2::SecurityGroupIngress
     end
   end
 end
